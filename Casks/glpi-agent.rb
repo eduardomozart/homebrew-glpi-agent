fc4b64ca9284b5c123367e2b02571003f45812c3
E <2974895+eduardomozart@users.noreply.github.com>
2026-07-31 01:32:40 -0300

    Initial commit

---
+++ Casks/glpi-agent.rb
@@ -0,0 +1,36 @@
+cask "glpi-agent" do
+  version "1.18"
+
+  if Hardware::CPU.intel?
+    sha256 "d644884155dda0f1798732f05f348e22abc126f5e1d6101b695dc48b14a70ffe"
+    url "https://github.com/glpi-project/glpi-agent/releases/download/#{version}/GLPI-Agent-#{version}_x86_64.pkg"
+    pkg "GLPI-Agent-#{version}_x86_64.pkg"
+  else
+    sha256 "5b3aca3df9c62ed1932bc1075d479e3c32cd45e5855a22f8722dbb0a3dff89e7"
+    url "https://github.com/glpi-project/glpi-agent/releases/download/#{version}/GLPI-Agent-#{version}_arm64.pkg"
+    pkg "GLPI-Agent-#{version}_arm64.pkg"
+  end
+
+  name "GLPI Agent"
+  desc "GLPI Agent for Mac OS X"
+  homepage "https://github.com/glpi-project/glpi-agent"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :catalina"
+
+  uninstall pkgutil: [
+              "com.teclib.glpi-agent",
+              "org.glpi-project.glpi-agent",
+            ],
+            delete:  [
+              "/opt/glpi-agent",
+            ],
+            launchctl: [
+              "org.glpi-project.glpi-agent",
+              "com.teclib.glpi-agent",
+            ]
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# homebrew-glpi-agent
+
+A Homebrew tap for [GLPI Agent](https://github.com/glpi-project/glpi-agent), allowing easy installation on macOS.
+
+## Installation
+
+To add this tap and install the GLPI Agent, run:
+
+```bash
+brew tap eduardomozart/glpi-agent
+brew install --cask glpi-agent
+```
+
+## Updates
+
+The cask is automatically updated when a new release is available on the official GLPI Agent repository via GitHub Actions.
+
+## Architectures
+
+This cask supports both Intel (x86_64) and Apple Silicon (arm64) architectures and will automatically download the correct package for your system.
